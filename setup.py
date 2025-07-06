8110a1b3dfb73eb308fd08e136db3f1a1fa92c27
thisisZoya <zoya.zq.210@gmail.com>
2025-07-06 14:38:13 +0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fixer-demo',
+    version='0.1',
+    description='Fixer service demo package',
+    url='#',
+    author='Zoya', 
+    author_email='zoya.zq.210@gmail.com',
+    license='MIT',
+    packages=['fixer'],
+    zip_safe=False)
