5dfcc8f28283eb0658ea1318ca939571e8f6b968
thisisZoya <zoya.zq.210@gmail.com>
2025-07-06 15:30:25 +0400

    Bump to version2

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='fixer-demo',
-    version='0.1',
+    version='0.2',
     description='Fixer service demo package',
     url='https://github.com/thisisZoya/fixer-demo',
     author='Zoya', 
     author_email='zoya.zq.210@gmail.com',
     license='MIT',
     packages=['fixer'],
+    install_requires=['requests'], 
     zip_safe=False)
